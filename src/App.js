989ac4a494cbd6c134c6daff041dcbd3fa7c275d
Abhinandan-Singh <abhinandansingh607@gmail.com>
2022-03-29 15:06:52 +0530

    lazy and suspense commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
-import {StrictMode, useState} from 'react';
+import { StrictMode, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
-import SearchParams from "./SearchParams";
-import ThemeContext from './ThemeContext';
-import Details from './Details';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+// import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
+// import Details from './Details';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
-
   const theme = useState("crimson");
 
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt me</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details/>
-            </Route>
-            <Route path="/">
-              <SearchParams/>
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>Loading Components</h2>}>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1>Adopt me</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
-}
+};
 
-ReactDOM.render(<StrictMode><App/></StrictMode>, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
