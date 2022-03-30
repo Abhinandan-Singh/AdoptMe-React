806310235412fa8e6055f537c44b69f431816a71
Abhinandan-Singh <abhinandansingh607@gmail.com>
2022-03-30 13:35:03 +0530

    lazy and suspense on modals

--- src/Details.js
+++ src/Details.js
@@ -1,76 +1,81 @@
-import { Component } from "react";
+import { Component, lazy } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
-import Modal from "./Modal";
 
-class Details extends Component{
+const Modal = lazy(() => import("./Modal"));
 
-    state = {loading:true, showModal:false};
+class Details extends Component {
+  state = { loading: true, showModal: false };
 
-    async componentDidMount(){
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
-        );
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
 
-        const json = await res.json();
+    const json = await res.json();
 
-        this.setState(
-            Object.assign({
-                loading:false
-            },
-            json.pets[0]
-            )
-        );
-    }
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
 
-    toggleModal = () => this.setState({showModal: !this.state.showModal});
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
 
-    adopt = () => (window.location = "https://thepetnest.com/adopt-a-pet")
+  adopt = () => (window.location = "https://thepetnest.com/adopt-a-pet");
 
-    render(){
-        const {animal, breed, city, state, description, name, images, showModal} = this.state;
-        return (
-            <div className="details">
-                <Carousel images = {images}/>
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{animal} - {breed} - {city}, {state}</h2>
-                    <ThemeContext.Consumer>
-                        {
-                            ([theme]) => (
-                                <button onClick={this.toggleModal} style={{backgroundColor:theme}}>Adopt {name}</button>
-                            )
-                        }
-                    </ThemeContext.Consumer>
-                    
-                    <p>{description}</p>
-                    {
-                        showModal ? (
-                            <Modal>
-                                <div>
-                                    <h1>Would you like to adopt {name}?</h1>
-                                    <div className="buttons">
-                                        <button onClick={this.adopt}>Yes</button>
-                                        <button onClick={this.toggleModal}>No</button>
-                                    </div>
-                                </div>
-                            </Modal>
-                        ): null
-                    }
+  render() {
+    const { animal, breed, city, state, description, name, images, showModal } =
+      this.state;
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
+
+          <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
                 </div>
-            </div>
-        )
-    }
+              </div>
+            </Modal>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 const DetailsWithRouter = withRouter(Details);
 
-export default function DetailsWithErrorBoundary(){
-    return (
-        <ErrorBoundary>
-            <DetailsWithRouter/>
-        </ErrorBoundary>
-    )
-};
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
